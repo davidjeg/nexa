dc60a4b95072c2ddb0d3244903811c45c9350f59
davidjeg <davideg96@outlook.com>
2026-08-11 20:42:38 -0500

    fixed proxy

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,26 @@
 import { NextResponse, type NextRequest } from "next/server";
 import { createClient } from "./app/lib/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const { supabase, supabaseResponse } = createClient(request);
+
   const {
     data: { user },
   } = await supabase.auth.getUser();
+
   const pathname = request.nextUrl.pathname;
+
   if (!user && pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-  if ((user && pathname === "/login") || pathname === "/register") {
+
+  if (user && (pathname === "/login" || pathname === "/register")) {
     return NextResponse.redirect(new URL("/", request.url));
   }
+
   return supabaseResponse;
 }
+
 export const config = {
-  matcher: ["/dashboard/:path", "/login", "/register"],
+  matcher: ["/dashboard/:path*", "/login", "/register"],
 };
